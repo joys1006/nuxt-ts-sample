50afbd1625629105580e2e3f67d258be4ac834af
joys1006 <joys1006@naver.com>
2021-11-27 20:35:17 +0900

    [REMOVE] java-server

--- java-server/src/main/java/com/toy/javaserver/api/configuration/WebMvcConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.toy.javaserver.api.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfiguration implements WebMvcConfigurer {
-}
